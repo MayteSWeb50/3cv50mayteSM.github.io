3346f3f1f3caa82cdd9a2c8a88f9efbebb1fde61
MayteSWeb50 <84929414+MayteSWeb50@users.noreply.github.com>
2021-05-27 12:51:06 -0500

    Update and rename inti.js to init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*Initializa Firebase con la configuracion del proecto. Revisa la 
+* configuracion en tu servidor Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+	apiKey: "AIzaSyAYE0q0K1vouNoEAilJ_uEd4UlqJuDfANI",
+    authDomain: "fbpw50auth.firebaseapp.com",
+    projectId: "fbpw50auth",
+    storageBucket: "fbpw50auth.appspot.com",
+    messagingSenderId: "142188620156",
+    appId: "1:142188620156:web:8f57b138334abde6059cf9",
+    measurementId: "G-C2T6DHDE5F"
+});
